d3dc0cd204d566d4a6670bbf9c0348aced0c8796
Aleksei Mialkin <aleksei@mialkin.ru>
2024-11-12 15:46:06 +0300

    Create initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Memory cache
+
+Showcase project for [↑ cache in memory](https://learn.microsoft.com/en-us/aspnet/core/performance/caching/memory).
+
+## Prerequisites
+
+- [↑ .NET SDK 8](https://dotnet.microsoft.com/en-us/download/dotnet/8.0)
+
+## Running application
+
+```bash
+dotnet watch --project src/MemoryCache.Api --no-hot-reload
+```
---
+++ src/MemoryCache.Api/Program.cs
@@ -0,0 +1,28 @@
+using Scalar.AspNetCore;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog(
+    (context, configuration) =>
+    {
+        configuration.ReadFrom.Configuration(context.Configuration);
+        configuration.WriteTo.Console();
+    });
+
+var services = builder.Services;
+
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+var application = builder.Build();
+
+application.UseSerilogRequestLogging();
+
+application.UseSwagger(options => { options.RouteTemplate = "openapi/{documentName}.json"; });
+application.MapScalarApiReference(x => { x.Title = "MemoryCache API"; });
+application.MapGet("/", () => Results.Redirect("/scalar/v1")).ExcludeFromDescription();
+
+application.MapGet("/say-hello", () => "Hello world!");
+
+application.Run();
